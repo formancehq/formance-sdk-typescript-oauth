d75830a913c9106d7e1c877c7818cea46a108771
Antoine Wacheux <antoine.wacheux@formance.com>
2024-01-09 15:28:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# @formance/formance-sdk-oauth
+
+This package provides a simple way to authenticate with the [Formance](https://formance.com) API.
+
+## Installation
+
+```bash
+npm install @formance/formance-sdk-oauth
+```
+
+## Usage
+
+```typescript
+import { SDK as Formance } from "@formance/formance-sdk";
+import { createAuthorizationProvider } from "@formance/formance-sdk-oauth"
+
+const FORMANCE_ENDPOINT = "https://xxxxxxxxxxxx-xxxxx.sandbox.formance.cloud"
+
+const formance = new Formance({
+    security: createAuthorizationProvider({
+        endpointUrl: FORMANCE_URL,
+        clientId: "<OAuth client id>",
+        clientSecret: "<OAuth client secret>",
+    }),
+    serverURL: FORMANCE_URL,
+});
+```
+
+## API Reference
+
+### `createAuthorizationProvider`
+
+Return a function that can be used to return an authorization header for authenticating with the Formance API. Internally, it uses the [OAuth 2.0 Client Credentials Grant](https://tools.ietf.org/html/rfc6749#section-4.4) with no scope to obtain an access token.
+
+#### Parameters
+
+| Name | Type | Description |
+| --- | --- | --- |
+| `options` | `OAuthOptions` | Options for the OAuth provider. |
+
+`OAuthOptions` reference
+
+| Name | Type | Description |
+| --- | --- | --- |
+| `endpointUrl` | `string` | The URL of the Formance API for your stack. |
+| `clientId` | `string` | The OAuth client id. |
+| `clientSecret` | `string` | The OAuth client secret. |
+| `tolerance?` | `number` | The number of seconds to allow for clock skew. Defaults to 5 minutes. |
+
+#### Returns
+
+A function that can be used to return an authorization header for authenticating with the Formance API.
+
---
+++ src/oauth.ts
@@ -0,0 +1,72 @@
+import { z } from 'zod';
+
+const WellKnownSchema = z.object({
+    token_endpoint: z.string(),
+});
+
+const TokenResponseSchema = z.object({
+    access_token: z.string(),
+    expires_in: z.number(),
+    token_type: z.string(),
+});
+
+export interface AuthorizationProviderOptions {
+    endpointUrl: string,
+    clientId: string,
+    clientSecret: string,
+    tolerance?: number
+}
+
+/**
+ * Creates an access token provider that uses the OAuth 2.0 client credentials flow.
+ * 
+ * This function returns a function that can be used to retrieve an access token on-demand. The access token is cached
+ * and refreshed automatically when it expires.
+ * 
+ * @param endpointUrl The URL of the formance stack you are using.
+ * @param clientId The client ID of the OAuth 2.0 client.
+ * @param clientSecret The client secret of the OAuth 2.0 client.
+ * @param tolerance The number of milliseconds to subtract from the token expiry time. This is used to ensure that the
+ *                  token is refreshed before it expires. It takes into account a potential clock skew between the client
+ *                  and the server, or a delay in the network. Defaults to 5 minutes.
+ */
+export function createAuthorizationProvider({endpointUrl, clientId, clientSecret, tolerance = 5*300*1000}: AuthorizationProviderOptions) {
+    const wellKnownUrl = `${endpointUrl}/api/auth/.well-known/openid-configuration`;
+
+    let accessToken: string | null = null;
+    let refreshAt: number | null = null;
+
+    return async function accessTokenProvider(): Promise<{ authorization: string }> {
+        // If we have a valid access token, return it
+        if (accessToken && refreshAt && refreshAt > Date.now()) {
+            return {
+                authorization: `Bearer ${accessToken}`,
+            };
+        }
+
+        // Retrieve the token endpoint from the well-known endpoint
+        const response = await fetch(wellKnownUrl);
+        const wellKnown = WellKnownSchema.parse(await response.json());
+
+        // Request a new access token
+        const tokenResponse = await fetch(wellKnown.token_endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                grant_type: 'client_credentials',
+                client_id: clientId,
+                client_secret: clientSecret,
+            }),
+        });
+
+        const token = TokenResponseSchema.parse(await tokenResponse.json());
+        accessToken = token.access_token;
+        refreshAt = Date.now() + token.expires_in * 1000 - tolerance;
+
+        return {
+            authorization: `Bearer ${accessToken}`,
+        };
+    }
+}
